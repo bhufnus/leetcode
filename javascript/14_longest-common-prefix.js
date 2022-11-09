2ed6c59ba6e8991a40a34a0e672e037e2af9b6ca
Brian Hufnus <brianhufnus@northwesternmutual.com>
2022-11-08 19:42:47 -0600

    remove elements

--- javascript/14_longest-common-prefix.js
+++ javascript/14_longest-common-prefix.js
@@ -1,6 +1,7 @@
 /**
- * @param {string[]} strs
- * @return {string}
+  Write a function to find the longest common prefix string amongst an array of strings.
+
+  If there is no common prefix, return an empty string "".
  */
 
 var longestCommonPrefix = function (strs) {
