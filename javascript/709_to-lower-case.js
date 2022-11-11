ba4c3b55c251373b85fb1b7ece622b0c546e2f75
Brian Hufnus <brianhufnus@northwesternmutual.com>
2022-11-11 15:07:22 -0600

    to lower case

---
+++ javascript/709_to-lower-case.js
@@ -0,0 +1,11 @@
+/*
+    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+ */
+
+var toLowerCase = function (s) {
+  return s.toLowerCase();
+};
+
+console.log(toLowerCase("HELLO"));
+
+// Expected : hello
